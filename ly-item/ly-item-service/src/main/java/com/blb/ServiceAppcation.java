7bbe7d920182d4102351d0bed45318a7defb0175
Yin <1374029384@qq.com>
2020-01-02 20:12:46 +0800

    2019.1.2 第二次提交

--- ly-item/ly-item-service/src/main/java/com/blb/ServiceAppcation.java
+++ ly-item/ly-item-service/src/main/java/com/blb/ServiceAppcation.java
@@ -3,7 +3,9 @@ package com.blb;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import tk.mybatis.spring.annotation.MapperScan;
 
+@MapperScan("com.blb.item.mapper")
 @EnableDiscoveryClient
 @SpringBootApplication
 public class ServiceAppcation {
